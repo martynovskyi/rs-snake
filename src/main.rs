fa9507e8d29f79b7cd764ffb070f96e97a6234d8
Vitalii Martynovskyi <vitalii@motokyi.com>
2025-06-16 14:25:27 +0300

    feat: validation of exit from grid

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use nannou::prelude::*;
-    
+
+mod snake_struct;
+use snake_struct::Point;
+
 struct Model {
     pub lines: i32,
     pub columns: i32,
@@ -7,10 +10,6 @@ struct Model {
     pub debug: bool,
 }
 
-pub struct Point {
-    pub x: i32,
-    pub y: i32,
-}
 
 const CELL_SIZE: f32 = 20.0;
 
@@ -22,7 +21,7 @@ fn main() {
 
 fn model(_app: &App) -> Model { 
     _app.new_window()
-        .size(1000, 1000)
+        .size(1080, 1080)
         .title("Snake")
         .view(view)
         .key_pressed(key_pressed)
@@ -44,34 +43,30 @@ fn event(_app: &App, _model: &mut Model, _e: Event) {
 fn view(_app: &App, _model: &Model, _frame: Frame) {
     let draw = _app.draw();
     let wr = _app.window_rect();
-
+    let snake = &_model.head;
     draw.background().color(PLUM);
+
+    if snake.x < 0 || snake.x >= _model.columns || snake.y < 0 || snake.y >= _model.lines {
+        println!("GAME OVER");
+    }
 
     let mut cell = Rect::from_w_h(CELL_SIZE, CELL_SIZE).top_left_of(wr);
     let mut head = cell;
     for line in 0.._model.lines {
         for col in 0.._model.columns {
-            draw.rect()
-                .no_fill()
-                .stroke_color(GRAY)
-                .stroke_weight(1.0)
-                .wh(cell.wh())
-                .xy(cell.xy());
+            draw_cell(&draw, &cell);
             if _model.debug {
-                // debug coordiantes
+             // debug coordiantes
                 draw.text(&format!("{},{}", line, col))
                     .color(BLACK)
                     .font_size(8)
                     .xy(cell.xy());
             }
 
-            if _model.head.x == col && _model.head.y == line {
-                draw.rect()
-                .color(BLACK)
-                .w(CELL_SIZE - 4.0)
-                .h(CELL_SIZE - 4.0)
-                .xy(cell.xy());
+            if snake.x == col && snake.y == line {
+                draw_snake_el(&draw, &cell);
             }
+        
             if col == _model.columns - 1 {
                cell = head.below(head);
             } else {
@@ -81,6 +76,23 @@ fn view(_app: &App, _model: &Model, _frame: Frame) {
         head = cell; 
     }
     draw.to_frame(_app, &_frame).unwrap();
+}
+
+fn draw_cell(draw: &Draw, cell: &Rect) {
+    draw.rect()
+        .no_fill()
+        .stroke_color(GRAY)
+        .stroke_weight(1.0)
+        .wh(cell.wh())
+        .xy(cell.xy());
+}
+
+fn draw_snake_el(draw: &Draw, cell: &Rect){
+    draw.rect()
+        .color(BLACK)
+        .w(CELL_SIZE - 4.0)
+        .h(CELL_SIZE - 4.0)
+        .xy(cell.xy());
 }
 
 
@@ -103,5 +115,5 @@ fn key_pressed(app: &App, model: &mut Model, key: Key) {
         }
         _other_key => {}
     }
-
+    println!("{:?} -> {:?}", key, model.head); 
 }
---
+++ src/snake_struct.rs
@@ -0,0 +1,6 @@
+
+#[derive(Debug)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+}
